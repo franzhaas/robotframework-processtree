c966d6c7db95236ca4c21a5a547c9017543b6c47
Franz Haas <franz.dominik.haas@gmail.com>
2025-02-26 14:18:26 +0100

    - intermediate

--- src/rf_processtree/__init__.py
+++ src/rf_processtree/__init__.py
@@ -15,22 +15,17 @@ from multiprocessing import Queue, JoinableQueue
 
 
 @dataclass
-class _queue_bewtween_processes_payload:
+class _QueueBewtweenProcessesPayload:
     link_forward: str
     link_back: str
     message: Any
 
 @dataclass
-class _logging_data:
+class _LoggingData:
     key: str
     dest: str
 
-def _make_ofile_safely(prefix: pathlib.Path, suffix: str):
-    handle, name = tempfile.mkstemp(prefix=str(prefix), suffix=suffix)
-    os.close(handle)
-    return name
-
-def _redirectionProcessWorker(q: JoinableQueue, odir: str, redirectionFile: pathlib.Path):
+def _redirection_process_worker(q: JoinableQueue, odir: str, redirection_file: pathlib.Path):
     redirects = {}
     while True:
         payload = q.get()
@@ -47,16 +42,16 @@ window.onload = function () {{
         document.body.innerHTML = '<h1>Error: broken link. likely this link points to something which has not materialised... Or you have overwritten the redirect file in a subsequent run</h1>';
     }}
 }};</script></head></html>""")
-        os.replace(name, odir / redirectionFile)
+        os.replace(name, odir / redirection_file)
         q.task_done()
 
 
 @library(listener='SELF')
 class rf_processtree():
-    _linkRedirectQueue: Optional[JoinableQueue] = None
-    _linkRedirectFileName: str = "redirect.html"
-    _writeQueue: Optional[Queue]  = None
-    _readQueue: Optional[Queue] = None
+    _link_redirect_queue: Optional[JoinableQueue] = None
+    _link_redirect_file_name: str = "redirect.html"
+    _write_queue: Optional[Queue]  = None
+    _read_queue: Optional[Queue] = None
 
     def __init__(self, target_suite: Optional[str] = None ):
         """Inititalizes either a parent or a child instance depending on the parameter
@@ -67,20 +62,21 @@ class rf_processtree():
         with contextlib.suppress(BuiltIn.RobotNotRunningError):
             self._child_starter = mp.get_context("spawn")
             self._target_suite = target_suite
-            self._logFilePath = BuiltIn.BuiltIn().get_variable_value("${LOG_FILE}")
-            self._oDir = pathlib.Path(BuiltIn.BuiltIn().get_variable_value("${OUTPUTDIR}")+"/")
+            self._log_file_path = BuiltIn.BuiltIn().get_variable_value("${LOG_FILE}")
+            self._o_dir = pathlib.Path(BuiltIn.BuiltIn().get_variable_value("${OUTPUTDIR}"))
             self._loglevel = BuiltIn.BuiltIn().get_variable_value("${LOG LEVEL}")
 
             if target_suite is None:
-                assert self._writeQueue is not None, "Parent did not provide a write queue"
-                assert self._readQueue is not None, "Parent did not provide a read queue"
-                assert self._linkRedirectQueue is not None, "Parent did not provide a link redirect queue"
+                assert self._write_queue is not None, "Parent did not provide a write queue"
+                assert self._read_queue is not None, "Parent did not provide a read queue"
+                assert self._link_redirect_queue is not None, "Parent did not provide a link redirect queue"
+                self._link_redirect_file_name = "../" + self._link_redirect_file_name
             else:
-                self._readQueue = self._child_starter.Queue()
-                self._writeQueue = self._child_starter.Queue()
-                if self._linkRedirectQueue is None:
-                    self.__class__._linkRedirectQueue = self._child_starter.JoinableQueue()
-                    self._child_starter.Process(target=_redirectionProcessWorker, args=(self._linkRedirectQueue, self._oDir, self._linkRedirectFileName), daemon=True).start()
+                self._read_queue = self._child_starter.Queue()
+                self._write_queue = self._child_starter.Queue()
+                if self._link_redirect_queue is None:
+                    self.__class__._link_redirect_queue = self._child_starter.JoinableQueue()
+                    self._child_starter.Process(target=_redirection_process_worker, args=(self._link_redirect_queue, self._o_dir, self._link_redirect_file_name), daemon=True).start()
         
     def start_keyword(self, data, result):
         self.id = result.id
@@ -92,14 +88,14 @@ class rf_processtree():
         Logging is generated which allows to follow a mesage to its destination.
         
         Limitations of the multiprocessing  Queues apply (only picklable objects can be sent)."""
-        assert self._linkRedirectQueue is not None, "Redirect system is not online, there is an issue with the linking of messages"
-        assert self._writeQueue is not None, "Interprocess communication queue is not online, there is an issue communicating between processes"
+        assert self._link_redirect_queue is not None, "Redirect system is not online, there is an issue with the linking of messages"
+        assert self._write_queue is not None, "Interprocess communication queue is not online, there is an issue communicating between processes"
         
-        payload = _queue_bewtween_processes_payload(link_forward=str(uuid.uuid4()), link_back=str(uuid.uuid4()), message=message)
-        self._linkRedirectQueue.put(_logging_data(key=payload.link_back, dest=f"{self._logFilePath}#{self.id}"))
-        self._writeQueue.put(payload)
-        logger.info(f"""Sending message to <a href="{self._linkRedirectFileName}#{payload.link_forward}">log</a>: {message}""", html=True)
-        self._linkRedirectQueue.join()
+        payload = _QueueBewtweenProcessesPayload(link_forward=str(uuid.uuid4()), link_back=str(uuid.uuid4()), message=message)
+        self._link_redirect_queue.put(_LoggingData(key=payload.link_back, dest=f"{self._log_file_path}#{self.id}"))
+        self._write_queue.put(payload)
+        logger.info(f"""Sending message to <a href="{self._link_redirect_file_name}#{payload.link_forward}">log</a>: {message}""", html=True)
+        self._link_redirect_queue.join()
 
     @keyword
     def receive_message(self, timeout: Optional[float] = None) -> Any:
@@ -108,12 +104,12 @@ class rf_processtree():
         Logging is generated which allows to trace the source of a mesage.
 
         Limitations of the multiprocessing  Queues apply (only picklable objects can be received)."""
-        assert self._linkRedirectQueue is not None, "Redirect system is not online, there is an issue with the linking of messages"
-        assert self._readQueue is not None, "Interprocess communication queue is not online, there is an issue communicating between processes"
-        payload = self._readQueue.get(block=True, timeout=timeout)
-        self._linkRedirectQueue.put(_logging_data(key=payload.link_forward, dest=f"{self._logFilePath}#{self.id}"))
-        logger.info(f"""Receiving message from <a href="{self._linkRedirectFileName}#{payload.link_back}">log</a>: {payload.message}""", html=True)
-        self._linkRedirectQueue.join()
+        assert self._link_redirect_queue is not None, "Redirect system is not online, there is an issue with the linking of messages"
+        assert self._read_queue is not None, "Interprocess communication queue is not online, there is an issue communicating between processes"
+        payload = self._read_queue.get(block=True, timeout=timeout)
+        self._link_redirect_queue.put(_LoggingData(key=payload.link_forward, dest=f"{self._log_file_path}#{self.id}"))
+        logger.info(f"""Receiving message from <a href="{self._link_redirect_file_name}#{payload.link_back}">log</a>: {payload.message}""", html=True)
+        self._link_redirect_queue.join()
         return payload.message
 
     @keyword
@@ -131,13 +127,8 @@ class rf_processtree():
         if this is not desired, implement a "we are done, lets go home ⊙" message.
         """
         assert self._target_suite is not None, "This is a child instance, cannot spawn another child process you can create a parent instance in a child suite though"
-        logfile = _make_ofile_safely(prefix=self._oDir / "log_", suffix=".html")
-        reportfile = _make_ofile_safely(prefix=self._oDir / "report_", suffix=".html")
-        consoleFile = _make_ofile_safely(prefix=self._oDir / "console_", suffix=".txt")
-        outXml =  _make_ofile_safely(prefix=self._oDir / "output_", suffix=".xml")
-
-        self._child_starter.Process(target=_spawner._start_child,
-                                    args=(self._target_suite, self._readQueue, self._writeQueue, self._linkRedirectQueue,
-                                            self._linkRedirectFileName, logfile, reportfile, consoleFile, outXml, self._loglevel),
-                                    daemon=True).start()
-        logger.info(f"""Started child process for suite {self._target_suite}, log file: <a href="{logfile}">log</a>, report file: <a href="{reportfile}">report</a>, console file: <a href="{consoleFile}">console</a>""", html=True)
+        _oDir = tempfile.TemporaryDirectory(dir=str(self._o_dir), delete=False).name
+        self._child_starter.Process(args=(self._target_suite, self._read_queue, self._write_queue, self._link_redirect_queue, self._link_redirect_file_name, _oDir,),
+                                    target=_spawner.start_child, daemon=True, kwargs={"loglevel":self._loglevel, "outputdir": self._o_dir}).start()
+        _oDir = pathlib.Path(_oDir).relative_to(self._o_dir)
+        logger.info(f"""Started child process for suite {self._target_suite}, log file: <a href="{_oDir}/log.html">log</a>, report file: <a href="{_oDir}/report.html">report</a>, console file: <a href="{_oDir}/console.log">console</a>""", html=True)
--- src/rf_processtree/_spawner.py
+++ src/rf_processtree/_spawner.py
@@ -1,13 +1,12 @@
-import io
 import robot
 import rf_processtree
 from multiprocessing import Queue, JoinableQueue
 
 
-def _start_child(target_suite: str, readQueue: Queue, writeQueue: Queue, redeirectQueue: JoinableQueue, redirectFileName: str, logfile: str, reportfile: str, consoleFile: str, outXml: str, loglevel):
-    console = io.TextIOWrapper(open(consoleFile, "wb", buffering=0), write_through=True)
-    rf_processtree.rf_processtree._linkRedirectQueue = redeirectQueue
-    rf_processtree.rf_processtree._linkRedirectFileName = redirectFileName
-    rf_processtree.rf_processtree._readQueue = writeQueue
-    rf_processtree.rf_processtree._writeQueue = readQueue
-    robot.run(target_suite, outputdir=".", loglevel=loglevel, log=logfile, report=reportfile, output=outXml, stdout=console)
+def start_child(target, read_queue: Queue, write_queue: Queue, redeirectQueue: JoinableQueue, redirectFileName: str, o_dir: str, **kwargs):
+    with open(f"{o_dir}/console.log", "w") as console:
+        rf_processtree.rf_processtree._link_redirect_queue = redeirectQueue
+        rf_processtree.rf_processtree._link_redirect_file_name = redirectFileName
+        rf_processtree.rf_processtree._read_queue = write_queue
+        rf_processtree.rf_processtree._write_queue = read_queue
+        robot.run(target, log=f"{o_dir}/log.html", report=f"{o_dir}/report.html", output=f"{o_dir}/output.xml", stdout=console, **kwargs)
